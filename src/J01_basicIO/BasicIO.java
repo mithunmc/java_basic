db4799e062b62400ae3770e6d05936c54f19d928
mitunmc <mithunmc@outlook.com>
2019-11-28 23:25:10 +0530

    Initial commit

---
+++ src/J01_basicIO/BasicIO.java
@@ -0,0 +1,14 @@
+package J01_basicIO;
+
+import java.util.Scanner;
+
+public class BasicIO {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer a: ");
+        int a = input.nextInt();
+        System.out.println("Enter a string b: ");
+        String b = input.next();
+        System.out.println("a: "+a+", b: "+b);
+    }
+}
