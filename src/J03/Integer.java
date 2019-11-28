0d09adb427b1754c9fc925216b15b119fb852ec5
mitunmc <mithunmc@outlook.com>
2019-11-28 23:54:21 +0530

    J03

---
+++ src/J03/Integer.java
@@ -0,0 +1,14 @@
+package J03;
+
+public class Integer {
+    public static void main(String[] args) {
+        int x = 123_456;
+        System.out.println("x: "+x);
+        int y = 0b110000;
+        System.out.println("y: "+y);
+        String s = "23";
+        int z = java.lang.Integer.parseInt(s);
+        System.out.println("z: "+z);
+    }
+
+}
